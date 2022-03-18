891399232c698dc11af9be37131d4d8a102c0e8c
Luis Garcia <luis.c.garcia@oracle.com>
2022-03-18 16:04:09 -0600

    1.1 Priori Analysis and Posteriori Testing@=

---
+++ 1.1_priori_posteriori.js
@@ -0,0 +1,18 @@
+// https://www.youtube.com/watch?v=-JTq1BFBwmo&list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O&index=2
+
+/* 
+PRIORI ANALYSIS
+- Algorith
+- Independent of language
+- Hardware Independent
+- Time & Space
+
+
+/*
+POSTERIORI ANALYSIS
+- Program
+- Language dependent
+- Hardware dependent
+- Watch time & bytes (time and space)
+*/
+
