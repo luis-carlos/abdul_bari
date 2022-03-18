dc96499230add9dd91fdf1b6437db01fa6ae93ea
Luis Garcia <luis.c.garcia@oracle.com>
2022-03-18 15:51:03 -0600

    Adding video playlist link and 1st lesson notes

---
+++ 1_intro_to_algorithms.js
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=0IAPZzGSbME&list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O&index=1
+
+/* 
+ALGORITHM
+- Design time
+- Domain Knowledge
+- Any language (English-like, Mathematical notation)
+- H/W & OS independent
+- Analyze
+*/
+
+/* 
+PROGRAM
+- Implementation time
+- Programmer
+- Programming language
+- H/W & OS dependent
+- Testing
+*/
