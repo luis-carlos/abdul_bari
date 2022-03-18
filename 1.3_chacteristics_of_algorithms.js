f27d577d9bb2660b22aeef2af19572e84df9b3e1
Luis Garcia <luis.c.garcia@oracle.com>
2022-03-18 16:57:52 -0600

    Self-explanatory

---
+++ 1.3_chacteristics_of_algorithms.js
@@ -0,0 +1,11 @@
+// https://www.youtube.com/watch?v=FbYzBWdhMb0&list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O&index=3
+
+/* 
+CHARACTERISTICS OF ALGORITHMS:
+- Input (0 or more)
+- Output (at least 1)
+- Definitenss (Single and exact meaning)
+- Finiteness
+- Effectiveness
+
+*/
